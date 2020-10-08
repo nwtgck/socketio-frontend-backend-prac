eb679d87e768462da4eb44bb4406af927cc8993a
Ryo Ota <nwtgck@nwtgck.org>
2020-10-08 23:19:32 +0900

    create simple multi-room chat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import * as http from 'http';
 import * as socketio from 'socket.io'
-import {events} from 'common';
+import {events, joinRoomReqType, talkType} from 'common';
 
 const port = 3000;
 const server = http.createServer((req, res) => {
@@ -11,10 +11,25 @@ io.on('connection', (socket) => {
   console.log('on connection');
 
   socket.on(events.join_room, (data: unknown) => {
-    console.log('join room: ', data);
+    const joinRoomReqEither = joinRoomReqType.decode(data);
+    if (joinRoomReqEither._tag === 'Left') {
+      console.log('invalid join room req:', data);
+      return;
+    }
+    const joinRoomReq = joinRoomReqEither.right;
+    socket.join(joinRoomReq.name);
+    console.log('room joined: ', joinRoomReq);
   });
 
-  socket.emit('event', 'this is an event');
+  socket.on(events.send_talk, (data: unknown) => {
+    const talkEither = talkType.decode(data);
+    if(talkEither._tag === 'Left') {
+      console.log('invalid talk:', data);
+      return;
+    }
+    const talk = talkEither.right;
+    io.to(talk.roomName).emit(events.receive_talk, talk);
+  });
 });
 server.listen(port, () => {
   console.log(`Listening on ${port}...`);
--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,35 +1,68 @@
 <template>
   <div>
-    Chat
+    <h1>Chat</h1>
+    <input v-model="roomName" placeholder="Room name" :disabled="joined">
+    <button @click="join" :disabled="joined">join</button>
+    <div v-if="joined">
+      <input type="text" v-model="talkBody">
+      <button @click="sendTalk">Send</button>
+      <div v-for="talk in talks">{{ talk.body }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import * as socketioClient from 'socket.io-client'
-import {events} from 'common';
+import {events, JoinRoomReq, Talk, talkType} from 'common';
+
+const endpoint = 'http://localhost:3000';
+const socket = socketioClient.connect(endpoint);
+const connectedPromise = new Promise(resolve => {
+  socket.on('connect', () => {
+    console.log('on connect');
+    resolve();
+  });
+});
+
+socket.on('disconnect', () => {
+  console.log('socket disconnected');
+});
 
 @Component
 export default class Chat extends Vue {
-  mounted() {
-    const endpoint = 'http://localhost:3000';
-    const socket = socketioClient.connect(endpoint);
+  private roomName: string = '';
+  private talks: Talk[] = [];
+  private joined: boolean = false;
+  private talkBody: string = '';
 
-    socket.on('connect', () => {
-      console.log('on connect');
+  async join() {
+    this.joined = true;
+    await connectedPromise;
+    const req: JoinRoomReq = {
+      name: this.roomName,
+    }
+    socket.emit(events.join_room, req);
 
-      socket.emit(events.join_room, {
-        name: 'room name 1'
-      });
+    socket.on(events.receive_talk, (data: unknown) => {
+      const talkEither = talkType.decode(data);
+      if(talkEither._tag === 'Left') {
+        console.log('invalid talk:', data);
+        return;
+      }
+      const talk = talkEither.right;
+      console.log('talk:', talk);
+      this.talks.unshift(talk);
     });
+  }
 
-    socket.on('event', (data: any) => {
-      console.log(`on event: ${data}`);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('socket disconnected');
-    });
+  sendTalk() {
+    const talk: Talk = {
+      roomName: this.roomName,
+      body: this.talkBody,
+    };
+    this.talkBody = '';
+    socket.emit(events.send_talk, talk);
   }
 }
 </script>
